4fd99a8c04ef18a84b77ae98f71091364bbf8e2b
Artem Kochuev <kochuev@trueconf.com>
2023-12-19 17:26:11 +0300

    Win build fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ void printTrace()
 	std::vector<void*> buffer(10);
 	int captured = tc::backtrace(buffer.data(), static_cast<int>(buffer.size()));
 	buffer.erase(std::begin(buffer) + captured, std::end(buffer));
-	std::unique_ptr<char*, std::function<void(void*)>> symbs = tc::backtraceSymbols(buffer.data(), std::size(buffer));
+	std::unique_ptr<char*, std::function<void(char**)>> symbs = tc::backtraceSymbols(buffer.data(), std::size(buffer));
 	for(int i = 0; i < std::size(buffer); ++i)
 	{
 		std::cout << symbs.get()[i] << std::endl;
